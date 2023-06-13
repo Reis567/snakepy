f6dccfdf9f764172554434ac847f32941f58914e
Reis567 <matheusdosreis9@gmail.com>
2023-06-12 21:12:15 -0300

    Menu

---
+++ README.md
@@ -0,0 +1,35 @@
+# Snake PY - By Reis567
+
+Este é um jogo da cobrinha implementado em Python utilizando a biblioteca Turtle.
+
+## Regras do Jogo
+
+1. O objetivo do jogo é controlar a cobra e comer as maçãs para crescer e marcar pontos.
+2. Use as teclas de seta para controlar a direção da cobra: cima, baixo, esquerda e direita.
+3. A cobra deve evitar colidir com as paredes ou consigo mesma, caso contrário, o jogo termina.
+4. Cada vez que a cobra come uma maçã, seu tamanho aumenta e a pontuação é incrementada.
+5. O jogo continua até que a cobra colida ou o jogador decida encerrar o jogo.
+
+## Como Jogar
+
+1. Certifique-se de ter o Python instalado em seu computador.
+2. Clone este repositório ou faça o download dos arquivos do jogo.
+3. Abra o terminal ou prompt de comando e navegue até o diretório do jogo.
+4. Execute o arquivo principal "main.py" com o comando `python main.py`.
+5. O jogo será iniciado e você poderá controlar a cobra usando as teclas de seta.
+6. Acompanhe sua pontuação na parte superior da tela.
+7. Divirta-se jogando!
+
+## Dependências
+
+- Python 3.x
+- Biblioteca Turtle
+
+## Créditos
+
+Este jogo foi desenvolvido por Reis567.
+
+<<<<<<< HEAD
+=======
+
+>>>>>>> 1f0b25f4757cb25079840680f3756b0730cc4074
--- menu.py
+++ menu.py
@@ -1,16 +1,42 @@
 import tkinter as tk
-from tkinter import messagebox , quit
-from snake import Cobra
-from apple import Maca
-from score import Pontuacao
+from tkinter import font
+import subprocess
 
-def jogar_novamente(tela):
-    resposta = messagebox.askyesno("Jogo da Cobra", "Deseja jogar novamente?")
-    if resposta:
-        resetar_jogo()
-    else:
-        tk.quit()
+def jogar():
+    subprocess.run(["python", "main.py"], cwd="./")
 
+janela = tk.Tk()
+janela.geometry("385x385")  # Define o tamanho da janela
 
+# Obter as dimensões da tela
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
 
+# Calcular a posição x e y para centralizar a janela
+pos_x = (largura_tela - 385) // 2
+pos_y = (altura_tela - 385) // 2
 
+janela.geometry(f"+{pos_x}+{pos_y}")  # Define a posição da janela
+janela.configure(bg="#005440")  # Define o background da janela como verde
+
+# Define uma fonte personalizada
+fonte_titulo = font.Font(family="Arial", size=20, weight="bold")
+fonte_botao = font.Font(family="Arial", size=12, weight="bold")
+
+# Título "Snake Game"
+titulo = tk.Label(janela, text="Snake Game", font=fonte_titulo, bg="#005440", fg="white")
+titulo.pack(pady=10)
+
+# Frame para centralizar os botões
+frame_botoes = tk.Frame(janela, bg="#005440")
+frame_botoes.pack()
+
+# Botão Jogar
+botao_jogar = tk.Button(frame_botoes, text="Jogar", command=jogar, bg="beige", font=fonte_botao, width=10, height=2, relief=tk.RAISED, bd=3)
+botao_jogar.pack(side=tk.LEFT, padx=10, pady=120)
+
+# Botão Sair
+botao_sair = tk.Button(frame_botoes, text="Sair", command=janela.quit, bg="beige", font=fonte_botao, width=10, height=2, relief=tk.RAISED, bd=3)
+botao_sair.pack(side=tk.LEFT, padx=10, pady=120)
+
+janela.mainloop()
